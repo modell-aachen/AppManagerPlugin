9c2d62dccaab5cd6876829c01d0655a706494a15
hennes <hennes@modell-aachen.de>
2015-10-23 17:48:24 +0200

    First Version of a single app container, with reading possible actions for each app
    Release-Note: none
    
    Ticket: 9304

--- pub/System/AppManager/appmanager.js
+++ pub/System/AppManager/appmanager.js
@@ -2,6 +2,18 @@
 	jQuery(document).ready(function() {
 		appManager.config();
 		appManager.loadTemplates();
+
+		var allTemplatesLoaded = setInterval(function() { 
+			if (
+					appManager.templates.singleAppContainerTmpl != '' &&
+					appManager.templates.singleAppActionsTmpl != ''
+				) {
+				clearInterval(allTemplatesLoaded);
+				jQuery.each(appManager.getDataObj(), function(key, value) {
+					appManager.createSingleAppContainer(appManager.templates.singleAppContainerTmpl, value);
+				});
+			}
+		}, 30);
 	});	
 
 	/***** AppManager Main Object *****/
@@ -10,8 +22,12 @@
 		/*********************************/
 		/***** AppManager Attributes *****/
 		/*********************************/
-		_dataObj 			: {},
-		topParentElem 		: {},
+		_dataObj 					: {},
+		topParentElem 				: {},
+		templates 					: {
+			singleAppContainerTmpl 	: '',
+			singleAppActionsTmpl	: ''
+		},
 
 		/******************************/
 		/***** AppManager Methods *****/
@@ -24,20 +40,34 @@
 
 		/***** Create a Container for a single App, displyed on the System Page *****/
 		createSingleAppContainer : function(template, obj) {
-			console.log(jQuery(template).find('[replaceVal="appname"]'))
-			console.log(this.topParentElem);
-
 			jQuery(this.topParentElem).append(template);
 			jQuery('.singleAppContainer:last').find('[replaceVal="appname"]').text(obj.appname);
 			jQuery('.singleAppContainer:last').find('[replaceVal="description"]').text(obj.description);
+
+			if (obj.actions.length > 0) {
+				jQuery('.singleAppContainer:last').find('.singleAppActionsNoContent').remove();
+				jQuery('.singleAppContainer:last').find('[replaceVal="actionsSelect"]').html(appManager.templates.singleAppActionsTmpl);
+
+				jQuery.each(obj.actions, function(key, value) {
+					jQuery('.singleAppContainer:last').find('.singleAppActionSelect').append('<option>' + value.name + '</option>');
+					jQuery('.singleAppContainer:last').find('.singleAppActionSelect option:last').data("meta", {desc : value.description});
+				});
+				jQuery('.singleAppContainer:last').find('.singleAppActionSelect').unbind().bind('change', function() {
+					var currentDesc = jQuery(this).find('option:selected').data("meta").desc;
+					jQuery(this).parent().parent().next().text(currentDesc);
+				});
+			} else {
+				jQuery('.singleAppContainer:last').find('.singleAppActionsContent').remove();
+			}
 		},
 
 		/***** Loads all necessary Templates for the AppManager *****/
 		loadTemplates : function() {
 			jQuery.get("AppManagerTemplates?contenttype=text/plain&skin=text&section=singleAppContainer", function(data, textStatus, XMLHttpRequest) {
-				jQuery.each(appManager.getDataObj(), function(key, value) {
-					appManager.createSingleAppContainer(data, value);
-				});
+				appManager.templates.singleAppContainerTmpl = data;
+			});
+			jQuery.get("AppManagerTemplates?contenttype=text/plain&skin=text&section=singleAppActions", function(data, textStatus, XMLHttpRequest) {
+				appManager.templates.singleAppActionsTmpl = data;
 			});
 		},
 
