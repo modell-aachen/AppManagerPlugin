501face8c30d0f39ea16d84e35d88a6c671295a2
Maik Glatki <glatki@modell-aachen.de>
2015-12-16 13:35:33 +0100

    Implement appaction REST handler, add ugly demonstrator.
    Ticket: #9304
    Release-Note: None

--- lib/Foswiki/Plugins/AppManagerPlugin.pm
+++ lib/Foswiki/Plugins/AppManagerPlugin.pm
@@ -30,8 +30,11 @@ sub initPlugin {
         return 0;
     }
 
-    my %restopts = (authenticate => 1, validate => 0, http_allow => 'POST');
-    Foswiki::Func::registerRESTHandler('install', \&_RESTinstall, %restopts);
+    # Workaround until a better interface comes around.
+    Foswiki::Func::registerTagHandler('AMPUGLY', \&_uglytable);
+
+    my %restopts = (authenticate => 1, validate => 0, http_allow => 'POST,GET');
+    Foswiki::Func::registerRESTHandler('appaction', \&_RESTappaction, %restopts);
 
     $restopts{http_allow} = 'GET';
     Foswiki::Func::registerRESTHandler('applist',   \&_RESTapplist,   %restopts);
@@ -39,15 +42,13 @@ sub initPlugin {
     return 1;
 }
 
+## Internal helpers
 # Returns application details
-sub _RESTappdetail {
-    my($session, $subject, $verb, $response) = @_;
-    my $q = $session->{request};
+sub _appdetail  {
+    my ( $app, @bad ) = @_;
+    die "Extra parameters in " . (caller(0))[3] if @bad;
+    map { confess "Mandatory parameter not defined in ".(caller(0))[3] unless defined $_} ( $app );
 
-    my $app = $q->param('name');
-    unless ($app) {
-        return encode_json({'error' => 'Parameter \'name\' is mandatory'});
-    }
     my $conf = _getJSONConfig($app);
     if ($conf) {
         my $res = {
@@ -68,21 +69,14 @@ sub _RESTappdetail {
         my $actions = {};
         if ($conf->{'install'}) { $actions->{install} = 'Install the application';}
         $res->{actions} = $actions;
-        return encode_json($res);
+        return $res;
     } else {
-        return encode_json({'error' => 'Not an application or application unmanaged'});
+        return {'error' => 'Not an application or application unmanaged'};
     }
 }
 
 # Returns list of managed and unmanaged applications.
-sub _RESTapplist {
-    my($session, $subject, $verb, $response) = @_;
-
-    # This page is only visible for the admin user
-    if (!Foswiki::Func::isAnAdmin()) {
-        return encode_json({'error' => 'Only Admins are allowed to list installed applications.'});
-    }
-
+sub _applist {
     my @topicList = grep {/AppContrib$/} Foswiki::Func::getTopicList('System');
 
     my $applist = {};
@@ -93,7 +87,7 @@ sub _RESTapplist {
             $applist->{$app} = 'unmanaged';
         }
     }
-    return encode_json($applist);
+    return $applist;
 }
 
 # Check for existing JSON file. Return undef on error.
@@ -136,115 +130,182 @@ sub _getJSONConfig {
     return $res;
 }
 
-# RestHandler to call _install routine for certain plugin
-# mode: check|install|forceinstall
-# app: appname
-# installname: optional name to install
-sub _RESTinstall {
-    my ($session, $subject, $verb, $response) = @_;
-    my $params = $session->{request}->{param};
-    my $args = {};
-    my $app;
-
-    if (exists $params->{mode}[0]) {
-        $args->{mode} = $params->{mode}[0];
-    } else {
-        $args->{mode} = 'check';
-    }
-    if (exists $params->{installname}[0]) {
-        $args->{installname} = $params->{installname}[0];
-    }
-    if (exists $params->{app}[0]) {
-        $app = $params->{app}[0];
-    } else {
-        $response->status( 400 );
-        return;
-    }
-
-    # Only Admins are allowed to install
-    if ((($args->{mode} eq 'install') or ($args->{mode} eq 'forceinstall')) and (!Foswiki::Func::isAnAdmin())) {
-        $response->status( 403 );
-        return;
-    }
-    my $conf = _getJSONConfig($app);
-    return encode_json(_install($conf, $args));
-}
-
-# Check if "install" routine in conf are possible, and if mode eq 'install', install.
-# conf parameter is mandatory,
-# args parameter is optional
-sub _install {
-    my ( $conf, $args, @bad ) = @_;
-    die "Extra parameters in " . (caller(0))[3] if @bad;
-    map { confess "Mandatory parameter not defined in ".(caller(0))[3] unless defined $_} ( $conf);
-
-    my $actions = $conf->{install};
-    my $installname = '';
-    my $mode = 'check';
-    if (exists $args->{installname} || exists $conf->{installname}) {
-        $installname = $args->{installname} || $conf->{installname};
-    }
-    if (exists $args->{mode}) {
-        $mode = $args->{mode};
-    }
-    # Return notices
-    my $res = [];
-    # Iterate all install routines;
-    for my $action (@$actions) {
-        # We are only interested in the first object, the actual action.
-        my $actA =  (keys $action)[0];
-        if ($actA eq 'move') {
-            # Iterate all files to move
-            my @toMove = @{$action->{'move'}};
-            # Take first key (should be only key from each object in move action
-            my $note =  "Installation will move files as follows:%BR%";
-            my @warnings = ();
-            # Iterate over move actions
-            my @passes = ('check');
-            if (($mode eq 'install') or ($mode eq 'forceinstall')) {
-                push @passes, $mode;
-            }
-            chdir _getRootDir();
-            for my $pass (@passes) {
-                # FIXME this duplication smells
-                for my $move (@toMove) {
-                    my ($src, $tar) = ((keys $move)[0], $move->{(keys $move)[0]});
-                    # Substitute place holders in paths
-                    if ($installname) {
-                        $src =~ s/%INSTALLNAME%/$installname/g;
-                        $tar =~ s/%INSTALLNAME%/$installname/g;
-                    }
-                    # Check existance of source and target files
-                    if ($pass eq 'check') {
-                        $note .= "$src to $tar%BR%";
-                        unless ( -e $src) {
-                            push @warnings, "Source file or directory $src does not exist!";
-                        }
-                        if ( -e $tar) {
-                            push @warnings, "Target file or directory $tar does already exist!";
-                        }
-                    } elsif ((($pass eq 'install') and (! scalar @warnings)) or ($pass eq 'forceinstall')) {
-                        Foswiki::Func::writeWarning("move $src to $tar.");
-                        File::Copy::move($src, $tar);
-                    }
-                }
-            }
-            push @$res, ['info', $note];
-            if (scalar @warnings) {
-                push @$res, map(['warning', $_], @warnings);
-            } elsif ($mode ne 'install') {
-                push @$res, ['can_install', '1'];
-            }
-        }
-    }
-    return $res;
-}
-
 # Return Foswiki root directory.
 sub _getRootDir {
     # FIXME there has to be a better solution
     return $Foswiki::cfg{TemplateDir} . '/..';
 }
+
+# Check if "install" routine in conf are possible, and if mode eq 'install', install.
+# $app is mandatory,
+# $args is optional
+sub _install {
+    my ( $name, $args, @bad ) = @_;
+    die "Extra parameters in " . (caller(0))[3] if @bad;
+    map { confess "Mandatory parameter not defined in ".(caller(0))[3] unless defined $_} ( $name);
+
+    my @apps;
+    if ($name eq 'all') {
+        my $applist = _applist();
+        while(my ($name, $status) = each %$applist) {
+            if ($status eq 'managed') {
+                my $detail = _appdetail($name);
+                if ($detail->{actions}->{install}) {
+                    push @apps, $name;
+                }
+            }
+        }
+    } else {
+        @apps = ($name);
+    }
+
+    my $results = {};
+    for my $app (@apps) {
+        my $conf = _getJSONConfig($app);
+        my $actions = $conf->{install};
+        my $installname = '';
+        my $mode = 'install';
+        if (exists $args->{installname} || exists $conf->{installname}) {
+            $installname = $args->{installname} || $conf->{installname};
+        }
+        if (exists $args->{mode}) {
+            $mode = $args->{mode};
+        }
+        # Return notices
+        my $res = [];
+        # Iterate all install routines;
+        for my $action (@$actions) {
+            # We are only interested in the first object, the actual action.
+            my $actA =  (keys $action)[0];
+            if ($actA eq 'move') {
+                # Iterate all files to move
+                my @toMove = @{$action->{'move'}};
+                # Take first key (should be only key from each object in move action
+                my $note =  "Installation will move files as follows:%BR%";
+                my @warnings = ();
+                # Iterate over move actions
+                my @passes = ('check');
+                if (($mode eq 'install') or ($mode eq 'forceinstall')) {
+                    push @passes, $mode;
+                }
+                chdir _getRootDir();
+                for my $pass (@passes) {
+                    # FIXME this duplication smells
+                    for my $move (@toMove) {
+                        my ($src, $tar) = ((keys $move)[0], $move->{(keys $move)[0]});
+                        # Substitute place holders in paths
+                        if ($installname) {
+                            $src =~ s/%INSTALLNAME%/$installname/g;
+                            $tar =~ s/%INSTALLNAME%/$installname/g;
+                        }
+                        # Check existance of source and target files
+                        if ($pass eq 'check') {
+                            $note .= "$src to $tar%BR%";
+                            unless ( -e $src) {
+                                push @warnings, "Source file or directory $src does not exist!";
+                            }
+                            if ( -e $tar) {
+                                push @warnings, "Target file or directory $tar does already exist!";
+                            }
+                        } elsif ((($pass eq 'install') and (! scalar @warnings)) or ($pass eq 'forceinstall')) {
+                            Foswiki::Func::writeWarning("move $src to $tar.");
+                            File::Copy::move($src, $tar);
+                        }
+                    }
+                }
+                push @$res, ['info', $note];
+                if (scalar @warnings) {
+                    push @$res, map(['warning', $_], @warnings);
+                } elsif ($mode ne 'install') {
+                    push @$res, ['can_install', '1'];
+                }
+            }
+        }
+        $results->{$app} = $res;
+    }
+    return $results;
+}
+
+# Ugly workaround, FIXME remove soon
+sub _uglytable {
+    my $res = '<table class="Modac_Standard" style="width: 60%;"><thead><tr><th>App</th><th>Desc</th><th>Action</th></thead>';
+    my $apps = _applist();
+    while(my ($name, $status) = each %$apps) {
+        if ($status eq 'managed') {
+            my $detail = _appdetail($name);
+            $res .= '<tr>';
+            $res .= '<td>';
+            $res .= $name;
+            $res .= '</td>';
+            $res .= '<td>';
+            $res .= $detail->{description};
+            $res .= '</td>';
+            $res .= '<td>';
+            for my $action (keys $detail->{actions}) {
+                $res .= '%BUTTON{"Action: ' . $action .'" href="%SCRIPTURLPATH{"rest"}%/AppManagerPlugin/appaction?name=' . $name . '&action=' . $action . '"}%';
+            }
+            $res .= '</td>';
+            $res .= '</tr>';
+        }
+    }
+    $res .= '<tr><td><b>Install all applications</b></td><td><b>Try to install all applications</b></td><td>%BUTTON{"Destroy Wiki" href="%SCRIPTURLPATH{"rest"}%/AppManagerPlugin/appaction?name=all&action=install"}%';
+    $res .= '</table>';
+    return $res;
+}
+
+## Registered handlers
+# Returns application details
+sub _RESTappdetail {
+    my($session, $subject, $verb, $response) = @_;
+    my $q = $session->{request};
+    my $app = $q->param('name');
+
+    # This page is only visible for the admin user
+    if (!Foswiki::Func::isAnAdmin()) {
+        return encode_json({'error' => 'Only Admins are allowed to use this.'});
+    }
+    unless ($app) { return {'error' => 'Parameter \'name\' is mandatory'}; }
+    return encode_json(_appdetail($app));
+}
+
+# Returns list of managed and unmanaged applications.
+sub _RESTapplist {
+    my ($session, $subject, $verb, $response) = @_;
+
+    # This page is only visible for the admin user
+    if (!Foswiki::Func::isAnAdmin()) {
+        return encode_json({'error' => 'Only Admins are allowed to list installed applications.'});
+    }
+    return encode_json(_applist());
+}
+
+# RestHandler to execute action for app
+# installname: optional name to install
+sub _RESTappaction {
+    my ($session, $subject, $verb, $response) = @_;
+    my $q = $session->{request};
+    my $name = $q->param('name');
+    my $action = $q->param('action');
+
+    # This page is only visible for the admin user
+    if (!Foswiki::Func::isAnAdmin()) {
+        return encode_json({'error' => 'Only Admins are allowed to execute actions.'});
+    }
+    unless ($name) { return encode_json({'error' => 'Parameter \'name\' is mandatory'}); }
+    unless ($action) { return encode_json({'error' => 'Parameter \'action\' is mandatory'}); }
+
+    # Check if action available
+    if ((_appdetail($name)->{actions}->{$action}) || ($action eq 'install' && $name eq 'all')) {
+        if ($action eq 'install') {
+            return encode_json(_install($name));
+        } else {
+        return encode_json({'error' => 'Action available, but no method defined.'});
+        }
+    } else {
+        return encode_json({'error' => 'Action not available for app.'});
+    }
+}
+
 
 1;
 
