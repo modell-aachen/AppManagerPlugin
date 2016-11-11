d72ac846e78bd559b3fdabce5950c1c7823713b5
Tobias Winkler <winkler@modell-aachen.de>
2016-11-11 07:19:06 +0100

    Added expert mode for editing install configs.
    Ticket: #15768
    Release-Note: none

--- dev/components/AppEdit.vue
+++ dev/components/AppEdit.vue
@@ -1,15 +1,12 @@
 <template>
     <div class="wrapper" v-show="ready">
-        Installname: <input type="text" v-model="config.destinationWeb"/>
-        <template v-if="hasCreateFormActions">
+        Installname
+        <input type="text" v-model="config.destinationWeb"/>
+        <template v-if="false">
             <p>The following forms will be installed:</p>
+            <div>
             <table class="ma-table">
-                <thead>
-                    <tr>
-                        <th>Form Name</th>
-                        <th>Form Group</th>
-                    </tr>
-                </thead>
+                <thead><tr><th>Form Name</th><th>Form Group</th></tr></thead>
                 <tbody>
                     <tr v-for="action in config.installActions">
                         <template v-if='action.action=="createForm"'>
@@ -19,9 +16,22 @@
                     </tr>
                 <tbody>
             </table>
+            </div>
         </template>
-        <button class="button primary" v-on:click="customInstall()" title="Install the app using this configuration">Install</button>
-        <button class="button alert" v-on:click="abort()" title="Discard changes">Abort</button>
+        <input type="checkbox" id="expertcheckbox" v-model="expert"/>
+        <label for="expertcheckbox">Expert</label>
+        <template v-if="expert">
+            <p>
+                <textarea rows="16" v-model="configAsJson" v-on:input="validateJson()"></textarea>
+            </p>
+            <p v-if="invalidJson" id="errorMessage">
+                {{ errorMessage }}
+            </p>
+        </template>
+        <p>
+            <button class="button primary" v-on:click="customInstall()" v-bind:disabled="invalidJson" title="Install the app using this configuration">Install</button>
+            <button class="button alert" v-on:click="abort()" title="Discard changes">Abort</button>
+        </p>
     </div>
 </template>
 
@@ -29,10 +39,14 @@
 
 export default {
     props: ['config'],
-    data : function () {
+    data: function () {
        return {
            ready: false,
-           hasCreateFormActions: false
+           expert: false,
+           configAsJson: "",
+           hasCreateFormActions: false,
+           invalidJson: false,
+           errorMessage: ""
        }
     },
     methods: {
@@ -41,7 +55,21 @@ export default {
         },
         customInstall: function() {
             this.$parent.$emit('customInstall', this.config);
-        }
+        },
+        validateJson: function() {
+            try {
+                var customConfig = JSON.parse(this.configAsJson);
+                this.config = customConfig; // causes vue warning
+                this.errorMessage = "";
+                this.invalidJson = false;
+            }
+            catch(e) {
+                window.console && console.log(e.message);               
+                window.console && console.log(e.name);
+                this.errorMessage = e.message;                 
+                this.invalidJson = true;           
+            }
+        }   
     },
     created: function() {
         // check if there are any createForm actions in the install config
@@ -51,10 +79,20 @@ export default {
                 break;
             }
         }
+        // make a json string out of the config object
+        this.configAsJson = JSON.stringify(this.config, null, '  ');
         this.ready = true;
     }
 }
 </script>
 
 <style lang="sass">
+    .flatskin-wrapped {
+        .redBorder {
+            border: 2px red solid;
+        }
+    }
+    #errorMessage {
+        color: red;
+    }
 </style>
